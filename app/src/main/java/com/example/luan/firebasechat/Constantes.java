eadbaeb2b443a291ef87a4d84bf4432416cf8c40
Luis <mtzvaldezluis@outlook.com>
2017-04-26 16:18:55 -0500

    Constants in project

---
+++ app/src/main/java/com/example/luan/firebasechat/Constantes.java
@@ -0,0 +1,14 @@
+package com.example.luan.firebasechat;
+
+/**
+ * Created by Luís on 26/04/2017.
+ */
+
+class Constantes {
+
+    static String NAME = "";
+
+    static String getNAME() {
+        return NAME;
+    }
+}
